3601da82db3bf64810ce278da9db480df6ceff14
Katherine Kent <k4tiek3nt1981@gmail.com>
2022-12-20 12:56:07 -0700

    Implemented a Root-Level Request Logger Middleware
    Implemented a Root-Level Request Logger Middleware

--- myApp.js
+++ myApp.js
@@ -2,6 +2,9 @@ var express = require('express');
 var app = express();
 console.log("Hello World");
 
+const path = require("path");
+const indexPath = path.resolve("./views/index.html");
+
 /* app.get("/", function(req, res) {
   res.send("Hello Express");
 }); */
@@ -12,9 +15,14 @@ app.use(express.static(__dirname + "/public"));
 // Assets at the /public route
 app.use("/public", express.static(__dirname + "/public"));
 
+app.use(function(req, res, next) {
+	let string = req.method + " " + req.path + " - " + req.ip;
+	console.log(string);
+	next();
+});
 
 app.get("/", function(req, res) {
-  res.sendFile(__dirname + "/views/index.html");
+  res.sendFile(indexPath); // changed from __dirname + "/views/index.html" to indexPath
 });
 
 app.get("/json", function(req, res) {
@@ -29,8 +37,6 @@ app.get("/json", function(req, res) {
     });
   }
 });
-
-
 
 
 
