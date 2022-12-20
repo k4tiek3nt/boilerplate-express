d0300b8bb27d80ccf579a6167255e6d970b916ee
Katherine Kent <k4tiek3nt1981@gmail.com>
2022-12-19 17:49:12 -0700

    Served the object {"message": "Hello json"}
    Served the object {"message": "Hello json"} as a response, in JSON format, to GET requests to the /json route.

--- myApp.js
+++ myApp.js
@@ -12,11 +12,16 @@ app.use(express.static(__dirname + "/public"));
 // Assets at the /public route
 app.use("/public", express.static(__dirname + "/public"));
 
+
 app.get("/", function(req, res) {
   res.sendFile(__dirname + "/views/index.html");
 });
 
-
+app.get("/json", function(req, res) {
+  res.json({
+    message: "Hello json"
+  });
+});
 
 
 
