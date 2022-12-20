c0d575fc4b6ab364b810f8971c6a3e56ef68910b
Katherine Kent <k4tiek3nt1981@gmail.com>
2022-12-19 17:40:05 -0700

    Mounted the express.static() middleware
    Mounted the express.static() middleware to the path /public with app.use().

--- myApp.js
+++ myApp.js
@@ -6,6 +6,12 @@ console.log("Hello World");
   res.send("Hello Express");
 }); */
 
+// Normal usage
+app.use(express.static(__dirname + "/public"));
+
+// Assets at the /public route
+app.use("/public", express.static(__dirname + "/public"));
+
 app.get("/", function(req, res) {
   res.sendFile(__dirname + "/views/index.html");
 });
